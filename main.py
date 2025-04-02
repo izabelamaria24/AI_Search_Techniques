f510b72f07e5676c18992b6ef7374e5cb66e6917
Iza <izabelajilavu@gmail.com>
2025-04-02 21:44:14 +0300

    modify structure

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ class KR:
             _, current = heapq.heappop(open_set)
             
             if current.index in scopeNodes:
-                # TODO
-                pass
+               return [current.index]
             
             for neighbor, cost in self.graph.get_neighbors(current):
                 score = g[current] + cost
@@ -41,7 +40,12 @@ class KR:
                         
             step_count += 1
             if step_count == n:
-                return open_set
+                # Display the heuristic values of nodes in the open_set
+                open_set_with_heuristic = [
+                    (item[1].index, self.heuristic(item[1], [self.graph.nodes[goal] for goal in scopeNodes]))
+                    for item in open_set
+                ]
+                return open_set_with_heuristic
         
         return None
 
@@ -82,6 +86,39 @@ class KR:
         path.pop()
         return None, min_threshold
     
+    
+    def solve_olimp(self, ID, start_index, scopeNodes, n, algorithm):
+        edges = [[1, 2, 3], [2, 3, 3], [1, 10, 3], [10, 22, 3], [22, 23, 3], [23, 24, 3], [24, 15, 3],
+            [15, 3, 3], [10, 11, 1], [11, 12, 1], [14, 15, 1], [14, 13, 1], [2, 5, 1], [5, 8, 1],
+            [17, 20, 1], [20, 23, 1], [4, 5, 2], [5, 6, 2], [19, 20, 2], [20, 21, 2], 
+            [4, 11, 2], [11, 19, 2], [6, 14, 2], [14, 21, 2], [7, 8, 1], [8, 9, 1],
+            [9, 13, 1], [13, 18, 1], [18, 17, 1], [17, 16, 1], [16, 12, 1], [12, 7, 1]]
+
+
+        coordinates = {
+            1: (0, 0), 2: (0, 3), 3: (0, 6), 4: (1, 1), 5: (3, 1), 6: (5, 1), 7: (2, 2), 8: (3, 2),
+            9: (4, 2), 10: (0, 3), 11: (1, 3), 12: (2, 3), 13: (4, 3), 14: (5, 3), 
+            15: (6, 3), 16: (2, 4), 17: (3, 4), 18: (4, 4), 19: (1, 5), 20: (3, 5),
+            21: (4, 5), 22: (0, 6), 23: (3, 6), 24: (6, 6)
+        }
+        
+        graph = Graph(edges, coordinates)
+        self.graph = graph
+        
+        if algorithm == "A*":
+            result = self.a_star(start_index, scopeNodes, n)
+            if len(result) == 1:
+                print(f"Scope node reached: {result[0]}")
+            else:
+                print("Open set with heuristic values:", sorted(result))
+        elif algorithm == "IDA*":
+            path = kr.IDA_star(start_index, scopeNodes, n)
+            if path:
+                print("Path found:", path)
+            else:
+                print("No path found.")
+    
+    
 
 class Node:
     def __init__(self, index, x, y):
@@ -111,32 +148,12 @@ class Graph:
         return self.adj_list.get(node.index, [])
 
 
-edges = [[1, 2, 3], [2, 3, 3], [1, 10, 3], [10, 22, 3], [22, 23, 3], [23, 24, 3], [24, 15, 3],
-        [15, 3, 3], [10, 11, 1], [11, 12, 1], [14, 15, 1], [14, 13, 1], [2, 5, 1], [5, 8, 1],
-        [17, 20, 1], [20, 23, 1], [4, 5, 2], [5, 6, 2], [19, 20, 2], [20, 21, 2], 
-        [4, 11, 2], [11, 19, 2], [6, 14, 2], [14, 21, 2], [7, 8, 1], [8, 9, 1],
-        [9, 13, 1], [13, 18, 1], [18, 17, 1], [17, 16, 1], [16, 12, 1], [12, 7, 1]]
-
-
-coordinates = {
-    1: (0, 0), 2: (0, 3), 3: (0, 6), 4: (1, 1), 5: (3, 1), 6: (5, 1), 7: (2, 2), 8: (3, 2),
-    9: (4, 2), 10: (0, 3), 11: (1, 3), 12: (2, 3), 13: (4, 3), 14: (5, 3), 
-    15: (6, 3), 16: (2, 4), 17: (3, 4), 18: (4, 4), 19: (1, 5), 20: (3, 5),
-    21: (4, 5), 22: (0, 6), 23: (3, 6), 24: (6, 6)
-}
-
-graph = Graph(edges, coordinates)
-kr = KR(graph)
-
-# n = 1
-# open_set = kr.a_star(12, [1, 20], n)
-# indexes = [item[1].index for item in open_set]
-# print(sorted(indexes))
-
-start_index = 12
-goal_indices = [1, 20]
-path = kr.IDA_star(start_index, goal_indices, n = 1)
-if path:
-    print("Path found:", path)
-else:
-    print("No path found.")
+if __name__ == "__main__":
+    ID = 620
+    start_index = 12
+    scopeNodes = [1, 20]
+    n = 1
+    algorithm = "A*"
+    
+    kr = KR(None)
+    kr.solve_olimp(ID, start_index, scopeNodes, n, algorithm)
